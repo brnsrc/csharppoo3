9e3c9cb76eaf230d01e13f6e6838cc1a24c5e7fc
Bruno Souza <bruno.costa@evertical.local>
2019-04-15 21:04:01 -0300

    inìnio do Projeto

---
+++ ByteBank/ParceiroComercial.cs
@@ -0,0 +1,35 @@
+﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ParceiroComercial
+    {
+        public string Senha { get; set; }
+        public bool Auntenticar(string senha)
+        {
+            return Senha == senha;
+        }
+
+        public bool Logar(ParceiroComercial funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.Auntenticar(senha);
+
+            if (usuarioAutenticado)
+            {
+                Console.WriteLine("Bem Vindo ao sistema !");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha Incorreta !");
+                return false;
+            }
+        }
+    }
+}
